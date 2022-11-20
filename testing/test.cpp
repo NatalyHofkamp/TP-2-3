f1ca960be6913ccf4d5ef5ce5d6ed950a3d77909
NatalyHofkamp <nhofkamp@udesa.edu.ar>
2022-11-20 20:29:45 -0300

    route testing

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,16 +1,39 @@
-#include "../googletest/googletest/include/gtest/gtest.h"
-#include "../include/City.h"
-#include "../include/Graph.h"
-#include "../include/Parser.h"
-#include "../include/Population.h"
-#include "../include/Reader.h"
-#include "../include/Route.h"
-#include "../include/Start.h"
 #include "../src/City.cpp"
 #include "../src/Graph.cpp"
-#include "../src/main.cpp"
 #include "../src/Parser.cpp"
 #include "../src/Population.cpp"
 #include "../src/Reader.cpp"
 #include "../src/Route.cpp"
 #include "../src/Start.cpp"
+#include <assert.h>
+
+void testCity(){
+    City *city = new City(0,1);
+    City *city2 = new City(0,1);
+    assert(city->DistCalc(city2) == 0);
+    
+}
+
+void testRute() {
+    Route* ruta= new Route();
+    City* city1= new City(0,3);
+    City* city2 = new City(4,0);
+    ruta->AddCity(city1);
+    ruta->AddCity(city2);
+    assert(ruta->TotalDist() == 10);
+    assert(ruta->GetSize() == 2);
+    assert(ruta->HasCity(new City(45,6)) == false);
+    assert(ruta->HasCity(city1) == true);
+    City* city3= new City(1,0);
+    City* city4 = new City(0,0);
+    ruta->AddCity(city3);
+    ruta->AddCity(city4);
+    assert(ruta->TotalDist() == 12);
+    assert(ruta->GetSize() == 4);
+    assert(ruta->HasCity(new City(25,6)) == false);
+    assert(ruta->HasCity(city4) == true);
+}
+int main(void){
+    testCity();
+    testRute();
+}
